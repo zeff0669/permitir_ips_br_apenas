d0478b2b087e1915d5de02baf2c55d7157e2690d
ZEFF <evandrogoetz5@gmail.com>
2024-04-30 18:09:38 -0300

    Update iptables_add_bripsv4.sh

--- iptables_add_bripsv4.sh
+++ iptables_add_bripsv4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read -r range; do
-  iptables -A INPUT -m iprange --src-range "$range" -j DROP
+  iptables -A INPUT -m iprange --src-range "$range" -j ACCEPT
 done < "/root/ipv4.txt"
