531ee079d5746577a5548813b3cea65ed14bb09b
ZEFF <evandrogoetz5@gmail.com>
2024-04-30 18:12:45 -0300

    Update and rename iptables_add_bripsv4.sh to iptables_add_brips.sh

--- iptables_add_brips.sh
+++ iptables_add_brips.sh
@@ -3,3 +3,7 @@
 while read -r range; do
   iptables -A INPUT -m iprange --src-range "$range" -j ACCEPT
 done < "/root/ipv4.txt"
+
+while read -r range6; do
+  iptables -A INPUT -m iprange6 --src-range6 "$range6" -j DROP
+done < "/root/ipv6.txt"
