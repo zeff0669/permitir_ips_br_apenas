93d88be18bed0180efe9a7e385a6418e41715776
ZEFF <evandrogoetz5@gmail.com>
2024-04-30 18:40:40 -0300

    Update iptables_add_brips.sh

--- iptables_add_brips.sh
+++ iptables_add_brips.sh
@@ -4,8 +4,12 @@ iptables -P INPUT DROP
 
 while read -r range; do
   iptables -A INPUT -m iprange --src-range "$range" -j ACCEPT
+  sed -i "2 a\iptables -A INPUT -m iprange --src-range '$range' -j ACCEPT" /etc/rc.local
 done < "/root/ipv4.txt"
 
 while read -r range6; do
   iptables -A INPUT -m iprange6 --src-range6 "$range6" -j ACCEPT
+  sed -i "2 a\iptables -A INPUT -m iprange6 --src-range6 '$range6' -j ACCEPT" /etc/rc.local
 done < "/root/ipv6.txt"
+
+sed -i "1 a\iptables -P INPUT DROP" /etc/rc.local
